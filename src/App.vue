7a3b25966bbca6cd451e9612916c86cc9ea2e5ce
Vadim <equipoise7@yandex.ru>
2024-08-02 14:52:43 +0300

    start project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>VUE Sneakers</h1>
+  <MyHello />
+</template>
+
+<script setup>
+import MyHello from './components/MyHello.vue'
+
+</script>
+
+<style scoped>
+body {
+  background-color: red;
+}
+</style>
---
+++ src/components/MyHello.vue
@@ -0,0 +1,10 @@
+<template>
+  <p>Я компонент MyHello</p>
+  <button>Open</button>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped></style>
